7e9aaad0fff665b634fd24ca0fbd830b7b721564
Jurjen N. E. Bos <jneb@users.noreply.github.com>
2021-04-02 17:30:20 +0200

    Update easter.py
    Some optimisations in readability
    Switched to New Scientist algorithm
    Removed bug in hiding Hanna's "easter egg"
    Added verbose option

--- easter.py
+++ easter.py
@@ -2,13 +2,11 @@
 """Program for calculating Good Friday and Easter for any given year between
 1900 and 2100.
 For Hanna's birthday, 2 april 2021.
-Using anonymous algorithm; see https://en.wikipedia.org/wiki/Computus
+Using New Scientist algorithm; see https://en.wikipedia.org/wiki/Computus
 """
 
-import argparse, datetime
-
-def days(n):
-    return datetime.timedelta(days=n)
+import argparse
+from datetime import timedelta, date
 
 def easter(Y):
     """The actual computation
@@ -17,17 +15,21 @@ def easter(Y):
     >>> easter(2021)
     datetime.date(2021, 4, 4)
     """
-    a = Y % 19
+    # write year as 400d + 100e + 4i + k
     b, c = divmod(Y, 100)
     d, e = divmod(b, 4)
-    f = (b + 8) // 25
-    g = (b - f + 1) //3
-    h = (19 * a + b - d - g + 15) % 30
     i, k = divmod(c, 4)
+    # slow shift over centuries: 6 for 1900-2100
+    g = (8 * b + 13) // 25
+    # 19 year moon cycle
+    a = Y % 19
+    # Probably nobody knows what's going on here
+    h = (19 * a + b - d - g + 15) % 30
     l = (32 + 2 * e + 2 * i - h - k) % 7
-    m = (a + 11 * h + 22 * l) // 451
-    month, day = divmod(h + l - 7 * m + 114, 31)
-    return datetime.date(Y, month, day + 1)
+    m = (a + 11 * h + 19 * l) // 433
+    month = (h + l - 7 * m + 90) // 25
+    day = (h + l - 7 * m + 33 * month + 19) % 32
+    return date(Y, month, day)
 
 def hanna():
     """Find dates from 1999 on where Good Friday is on April 2nd.
@@ -39,26 +41,39 @@ def hanna():
     2094
     """
     for y in range(1999, 2100):
-        if easter(y) == datetime.date(y, 4, 4): print(y)
+        if easter(y) == date(y, 4, 4): print(y)
 
-def main(args):
+def main(year, verbose=False):
     """Main program
     """
-    Y = args.year or datetime.date.today().year
+    Y = year or date.today().year
     if Y < 100: Y += 2000
     d = easter(Y)
     print(f"Easter day for year {Y}: {d:%d %B}")
-    print(f"Good friday: {d-days(2):%d %B}")
+    related = [
+        (-46, "Ash Wednesday"),
+        (-3, "Maundy Thursday"),
+        (-2, "Good friday"),
+        (-1, "Holy Saturday"),
+        (39, "Ascension"),
+        (49, "Pentecost"),
+        ]
+    if not verbose:
+        related = related[2:3]
+    for delta, name in related:
+        print(f"{name}: {d+timedelta(days=delta):%d %B}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Calculate good friday for a given year')
     parser.add_argument('year', metavar='Y', type=int, nargs='?',
         help='Year for which to calculate the good friday date')
+    parser.add_argument('-v', '--verbose', action='store_true',
+        help='Show all related Christian calendar days')
     parser.add_argument('-t', '--test', action='store_true',
         help='Self test')
     parser.add_argument('--hanna', action='store_true',
-        help='argparse.suppress')
+        help=argparse.SUPPRESS)
     args = parser.parse_args()
     if args.test:
         import doctest
@@ -66,4 +81,4 @@ if __name__ == "__main__":
     elif args.hanna:
         hanna()
     else:
-        main(args)
+        main(args.year, args.verbose)
